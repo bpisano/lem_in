e882d416de123142f01dd140555f1bd2c4389dad
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-03-16 11:46:37 +0100

    .h

--- lem-in.h
+++ lem-in.h
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/15 18:44:38 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/15 18:48:32 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/15 19:36:24 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,5 +15,39 @@
 # define LEM-IN_H
 
 # include "libft.h"
+
+typedef struct	s_start
+{
+	char		*name;
+	int			x;
+	int			y;
+	t_sal		**link;
+}				t_start;
+
+typedef struct	s_end
+{
+	char		*name;
+	int			x;
+	int			y;
+	t_sal		**link;
+}				t_end;
+
+typedef struct	s_sal
+{
+	char		*name;
+	int			x;
+	int			y;
+	t_sal		**next;
+	t_sal		**link;
+}				t_sal;
+
+typedef struct	s_lem
+{
+	t_start		*start;
+	t_end		*end;
+	char		command;
+	int			ants;
+	t_sal		*lst;
+}				t_lem;
 
 #endif
